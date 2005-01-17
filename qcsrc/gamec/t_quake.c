4c3610bac65d8be6dfa0ac16bd8b7e08e833fd97
vermeulenl <vermeulenl@f962a42d-fe04-0410-a3ab-8c8b0445ebaa>
2005-01-17 09:21:06 +0000

    small error

--- qcsrc/gamec/t_quake.c
+++ qcsrc/gamec/t_quake.c
@@ -1,18 +1,11 @@
 //***********************
-//QUAKE 3 ENTITIES - So people can play quake3 maps with the nexuiz weapons
+//QUAKE 1 ENTITIES - So people can play quake1 maps with the nexuiz weapons
 //***********************
-weapon_lightning
-weapon_nailgun
-weapon_supernailgun
-weapon_supershotgun 
-
-void weapon_lightning (void) {weapon_nex();}
 void weapon_nailgun (void) {weapon_electro();}
 void weapon_supernailgun (void) {weapon_hagar();}
 void weapon_supershotgun (void) {weapon_uzi();}
 
 void item_spikes (void) {item_bullets();}
-void item_armor1 (void) {item_armor25();}
 void item_armor2 (void) {item_armor25();}
 void item_armorInv (void) {item_armor25();}
 void item_health (void) {item_health25();}
