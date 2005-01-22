49359422ceff2b047fe5c72aa6ba74d97a144db5
lordhavoc <lordhavoc@f962a42d-fe04-0410-a3ab-8c8b0445ebaa>
2005-01-22 07:01:50 +0000

    added item_armor_shard, item_armor_body (?), and item_armor_combat to support more q3 map entities

--- qcsrc/gamec/t_quake3.c
+++ qcsrc/gamec/t_quake3.c
@@ -10,6 +10,9 @@ void item_slugs (void) {item_cells();}
 void item_lightning (void) {item_cells();}
 void item_armorshard (void) {item_armor1();}
 void item_armorbody (void) {item_armor25();}
+void item_armor_shard (void) {item_armor1();}
+void item_armor_body (void) {item_armor25();}
+void item_armor_combat (void) {item_armor25();}
 void item_health_small (void) {item_health1();}
 void item_health_large (void) {item_health25();}
 void item_health_mega (void) {item_health100();}
