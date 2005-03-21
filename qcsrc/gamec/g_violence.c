8cda9c253b316f2d951fbc4b09f766385299e54a
lordhavoc <lordhavoc@f962a42d-fe04-0410-a3ab-8c8b0445ebaa>
2005-03-21 10:24:40 +0000

    fixed 'eye gibbing repeatedly' bug (hopefully) and made gib code more consistent

--- qcsrc/gamec/g_violence.c
+++ qcsrc/gamec/g_violence.c
@@ -1,23 +1,28 @@
-void GibTouch ()
+void GibDamage (entity inflictor, entity attacker, float damage, float deathtype, vector hitloc, vector force)
 {
-	te_blood (self.origin + '0 0 1', '0 0 30', 10);
-
 	float r;
 	r = random ();
 	if (r < 0.60)
-			sound (self, CHAN_IMPACT, "misc/gib_splat01.wav", 1, ATTN_NORM);
+		sound (self, CHAN_IMPACT, "misc/gib_splat01.wav", 1, ATTN_NORM);
 	else if (r < 0.65)
-			sound (self, CHAN_IMPACT, "misc/gib_splat02.wav", 1, ATTN_NORM);
+		sound (self, CHAN_IMPACT, "misc/gib_splat02.wav", 1, ATTN_NORM);
 	else if (r < 0.70)
-			sound (self, CHAN_IMPACT, "misc/gib_splat03.wav", 1, ATTN_NORM);
+		sound (self, CHAN_IMPACT, "misc/gib_splat03.wav", 1, ATTN_NORM);
 	else if (r < 0.75)
-			sound (self, CHAN_IMPACT, "misc/gib_splat04.wav", 1, ATTN_NORM);
-	self.health = self.health - 11;
+		sound (self, CHAN_IMPACT, "misc/gib_splat04.wav", 1, ATTN_NORM);
+	te_blood (self.origin + '0 0 1', '0 0 30', 10);
+	self.health = self.health - damage;
 	if (self.health <= -12)
 	{
 		self.event_damage = nullfunction;
 		SUB_VanishOrRemove (self);
 	}
+}
+
+
+void GibTouch ()
+{
+	GibDamage (other, other, 11, 0, self.origin, '0 0 0');
 }
 
 // changes by LordHavoc on 03/30/04
@@ -42,7 +47,7 @@ void TossGib (entity gib, string mdlname, vector org, vector v, float destroyont
 	gib.health = -1;
 	gib.takedamage = DAMAGE_YES;
 	gib.damageforcescale = 3.5;
-	//gib.event_damage = GibDamage;
+	gib.event_damage = GibDamage;
 	if (destroyontouch == 1)
 		gib.touch = GibTouch;
 
