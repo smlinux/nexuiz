0907bb14c1ba9bb906c5a3e7dcad42b8e312ed7f
maikmerten <maikmerten@f962a42d-fe04-0410-a3ab-8c8b0445ebaa>
2005-09-07 15:56:54 +0000

    count login tries

--- qcsrc/gamec/clientcommands.c
+++ qcsrc/gamec/clientcommands.c
@@ -3,14 +3,17 @@ void SV_ParseClientCommand(string s) {
 	
 	if(cvar("sv_clientcommands")) {
 		if(argv(0) == "clogin") {
-			if(argv(1) == cvar_string("sv_clientcommands_password")) {
+			if(self.adminstatus < -5) {
+				sprint(self, "Too many unsuccessful tries.\n");			
+			} else if(argv(1) == cvar_string("sv_clientcommands_password")) {
 				self.adminstatus = 1;
 				sprint(self, "You now have remote admin status.\n");
 			} else {
 				sprint(self, "Wrong password.\n");
+				self.adminstatus--;
 			}
 		} else if(argv(0) == "ccmd") {
-			if(self.adminstatus)
+			if(self.adminstatus > 0)
 				localcmd(strcat(argv(1)," ",argv(2)," ",argv(3)," ",argv(4),"\n"));
 			else
 				sprint(self, "You don't have remote admin status.\n");
