9d6ad2243aed0a55a8543f6bcbf5f573d540b5f4
savagex <savagex@f962a42d-fe04-0410-a3ab-8c8b0445ebaa>
2005-11-11 19:16:00 +0000

    neutral team color for pickup weapons (this time hopefully working)

--- data/qcsrc/gamec/t_items.c
+++ data/qcsrc/gamec/t_items.c
@@ -131,9 +131,8 @@ void StartItem (string itemmodel, string pickupsound, float defaultrespawntime, 
 		setorigin (self, self.origin + '0 0 22');
 		setsize (self, '-12 -12 -12', '12 12 12');
 		
-		// FIXME: weapon pickups should have a proper 'team' color (something neutral)
-		// This would be the right place to do this
-		// doesn't work: setcolor(self,178.0);
+		// neutral team color for pickup weapons
+		self.colormap = 160 * 1024 + 160;
 	}
 	else
 	{
