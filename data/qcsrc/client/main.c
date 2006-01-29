0d729ac0fefa8eb17b5dfc3327411fa50674a804
kadaverjack <kadaverjack@f962a42d-fe04-0410-a3ab-8c8b0445ebaa>
2006-01-29 19:37:08 +0000

    add csqc skeleton for Nexuiz
    right now it doesn't do anything but tell dp to show crosshair, viewmodel and hud...
    known bugs:
      - doesn't work on dedicated servers (engine bug)
      - spectators can't move

---
+++ data/qcsrc/client/main.c
@@ -0,0 +1,98 @@
+float nxt_cmd;
+
+void SysParms_UPDATE (void)
+{
+	float f;
+
+	vid_width = cvar("vid_conwidth");
+	vid_height = cvar("vid_conheight");
+	makevectors(input_angles);
+	pl_view_forward = v_forward;
+	pl_view_right = v_right;
+	pl_view_up = v_up;
+	pl_xyspeed = floor(sqrt(pmove_vel_x*pmove_vel_x + pmove_vel_y*pmove_vel_y));
+
+	f = getstati(STAT_HEALTH);
+	if(f <= 0 && !(player_flags & PFL_DEAD))
+		player_flags |= PFL_DEAD;
+	else if(f > 0 && player_flags & PFL_DEAD)
+		player_flags -= player_flags & (PFL_DEAD | PFL_GIBBED);
+
+	if(nxt_cmd < time)
+	{
+		nxt_cmd = time + 5;
+		localcmd("r_showtris 0\n");
+	}
+
+}
+
+void CSQC_Ent_Update (float isnew)
+{
+	float i;
+	i = ReadByte();
+}
+
+void CSQC_Ent_Remove ()
+{
+	remove(self);
+}; 
+
+
+void CSQC_Init (void)
+{
+}
+
+void CSQC_Shutdown (void)
+{
+}
+
+void CSQC_ConsoleCommand (string str)
+{
+}
+
+void CSQC_UpdateView (void)
+{
+	SysParms_UPDATE();
+	View_Update();
+
+	R_ClearScene();
+
+	R_SetView(VF_DRAWWORLD, 1);
+
+	if(getstati(STAT_HEALTH) == 6666)
+	{
+		R_SetView(VF_DRAWCROSSHAIR, 0);
+		R_SetView(VF_DRAWENGINESBAR, 0);
+	}
+	else
+	{
+		R_SetView(VF_DRAWCROSSHAIR, 1);
+		R_SetView(VF_DRAWENGINESBAR, 1);
+	}
+	
+	R_AddEntities(MASK_NORMAL | MASK_ENGINE | MASK_ENGINEVIEWMODELS);
+	R_RenderScene();
+}
+
+float CSQC_InputEvent (float event, float parama)
+{
+	return false;
+}
+
+void CSQC_Parse_StuffCmd (string msg)
+{
+	// doesn't work, dp obviously doesn't execute csprogs.dat on demo playback
+	if(isdemo() && msg == "menu_showteamselect\n")
+		return;
+	localcmd(msg);
+}
+
+void CSQC_Parse_Print (string msg)
+{
+	print(msg);
+}
+
+void CSQC_Parse_CenterPrint (string msg)
+{
+	cprint(msg);
+}
---
+++ data/qcsrc/client/view.c
@@ -0,0 +1,41 @@
+float max_ups;
+vector angle_alive;
+
+void View_Update (void)
+{
+	float f;
+	string s;
+
+	vieworg = pmove_org;
+	angle_alive = input_angles;
+
+	if(player_flags & PFL_DEAD)
+	{
+		R_SetView(VF_ORIGIN, vieworg+'0 0 4');
+		angle_alive_z = 80;
+		R_SetView(VF_ANGLES, angle_alive);
+		R_SetView(VF_CL_VIEWANGLES, angle_alive);
+		return;
+	}
+
+	if(input_buttons & 16 && getstati(STAT_HEALTH) != 6666)
+		vieworg += PL_CROUCH_VIEW_OFS;
+	else
+		vieworg += PL_VIEW_OFS;
+
+	R_SetView(VF_ORIGIN, vieworg);
+	R_SetView(VF_ANGLES, input_angles);
+
+	if(!cvar("cg_showvelocity"))
+		return;
+
+	f = pl_xyspeed;
+	if(max_ups < f)
+		max_ups = f;
+	s = ftos(f);
+	s = strcat(s, " ups");
+	drawstring('1 0 0'*vid_width + '-64 64 0', s, '8 8 0', VEC_1, 1, 0);
+	s = ftos(max_ups);
+	s = strcat(s, " max");
+	drawstring('1 0 0'*vid_width + '-64 72 0', s, '8 8 0', VEC_1, 1, 0);
+}
