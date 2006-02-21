8070df55976f2e54d1eeca3f97f8e4021ca25ab3
savagex <savagex@f962a42d-fe04-0410-a3ab-8c8b0445ebaa>
2006-02-21 20:44:20 +0000

    replace the horribly-high-poly gibs with low poly ones

--- data/qcsrc/server/gamec/g_world.c
+++ data/qcsrc/server/gamec/g_world.c
@@ -37,6 +37,9 @@ void worldspawn (void)
 	precache_model ("models/gibs/gib4.md3");
 	precache_model ("models/gibs/gib5.md3");
 	precache_model ("models/gibs/gib6.md3");
+	precache_model ("models/gibs/gib1.mdl");
+	precache_model ("models/gibs/gib2.mdl");
+	precache_model ("models/gibs/gib3.mdl");		
 	precache_model ("models/grenademodel.md3");
 	precache_model ("models/hagarmissile.mdl");
 	precache_model ("models/items/a_bullets.mdl");
