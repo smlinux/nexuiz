449e12696b58d8020b2f44119e0fe47f8df5d19b
div0 <div0@f962a42d-fe04-0410-a3ab-8c8b0445ebaa>
2006-12-31 11:01:55 +0000

    new cvar vid_x11_display for use by nexuiz-linux-*.sh

--- nexuiz-linux-sdl.sh
+++ nexuiz-linux-sdl.sh
@@ -11,4 +11,80 @@ case "$(uname -m)" in
   *)		nexuiz="nexuiz-linux-686-sdl" ;;
 esac
 
-exec ./${nexuiz} "${@}"
+set -- ./${nexuiz} "${@}"
+
+xserver=
+xlayout=
+
+# now how do we execute it?
+if [ -r ~/.nexuiz/data/config.cfg ]; then
+	while read -r CMD KEY VALUE; do
+		VALUE=${VALUE#\"}
+		VALUE=${VALUE%\"}
+		case "$CMD:$KEY" in
+			seta:vid_x11_display)
+				case "$VALUE" in
+					:*)
+						;;
+					*)
+						VALUE=:$VALUE
+						;;
+				esac
+				VALUE="$VALUE "
+				xserver="${VALUE%% *}"
+				xserver=${xserver#:}
+				xlayout=${VALUE#* }
+				xlayout=${xlayout% }
+				;;
+		esac
+	done < ~/.nexuiz/data/config.cfg
+fi
+
+case "$xserver" in
+	'')
+		;;
+	*[!0-9]*)
+		echo "Not using display ':$xserver': evil characters"
+		;;
+	*)
+		msg=
+		lf='
+'
+		prefix=
+
+		# check for a listening X server on that socket
+		if netstat -nl | grep -F " /tmp/.X11-unix/X$xserver" >/dev/null; then
+			# X server already exists
+			export DISPLAY=:$xserver
+			prefix="DISPLAY=:$xserver "
+			msg=$msg$lf"- Running Nexuiz on already existing display :$xserver"
+		else
+			set -- startx "$@" -- ":$xserver"
+			msg=$msg$lf"- Running Nexuiz on a newly created X server :$xserver."
+			case "$xlayout" in
+				'')
+					;;
+				*[!A-Za-z0-9]*)
+					echo >&2 "Not using layout '$xlayout': evil characters"
+					xlayout=
+					;;
+				*)
+					set -- "$@" -layout "$xlayout"
+					msg=$msg$lf"- Using the ServerLayout section named $xlayout."
+					;;
+			esac
+		fi
+
+		echo "X SERVER OVERRIDES IN EFFECT:$msg"
+		echo
+		echo "Resulting command line:"
+		echo "  $prefix$*"
+		echo
+		echo "To undo these overrides, edit ~/.nexuiz/data/config.cfg and remove the line"
+		echo "starting with 'seta vid_x11_display'."
+		echo
+		echo
+		;;
+esac
+
+exec "$@"
