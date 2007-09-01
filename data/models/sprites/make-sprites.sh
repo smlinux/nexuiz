8846340ad2b8337690fd71fa725fa1b603ce7f58
div0 <div0@f962a42d-fe04-0410-a3ab-8c8b0445ebaa>
2007-09-01 21:30:35 +0000

    use r_labelsprites DP feature for sprites; add script that generates the sprites

--- data/models/sprites/make-sprites.sh
+++ data/models/sprites/make-sprites.sh
@@ -1,3 +1,19 @@
+width=256
+height=64
+
+sprwidth=256
+sprheight=64
+
+sprite()
+{
+	name=$1
+	color=$2
+	bgcolor=$3
+	text=$4
+	svgwidth=512
+	svgheight=128
+
+	cat <<EOF >$name.svg
 <?xml version="1.0" encoding="UTF-8" standalone="no"?>
 <!-- Created with Inkscape (http://www.inkscape.org/) -->
 <svg
@@ -12,18 +28,20 @@
    height="128"
    id="svg2"
    sodipodi:version="0.32"
-   inkscape:version="0.44"
+   inkscape:version="0.45"
    version="1.0"
    inkscape:export-filename="bluebase.tga"
    inkscape:export-xdpi="90"
    inkscape:export-ydpi="90"
    sodipodi:docname="template.svg"
-   sodipodi:docbase="/home/rpolzer/software/games/nexuiz/data/models/sprites">
+   sodipodi:docbase="/home/rpolzer/software/games/nexuiz/data/models/sprites"
+   inkscape:output_extension="org.inkscape.output.svg.inkscape"
+   sodipodi:modified="true">
   <defs
      id="defs4" />
   <sodipodi:namedview
      id="base"
-     pagecolor="black"
+     pagecolor="#$bgcolor"
      bordercolor="#666666"
      borderopacity="1.0"
      gridtolerance="10000"
@@ -46,7 +64,7 @@
      showguides="true"
      inkscape:guide-bbox="true"
      inkscape:window-width="849"
-     inkscape:window-height="579"
+     inkscape:window-height="590"
      inkscape:window-x="198"
      inkscape:window-y="66" />
   <metadata
@@ -65,19 +83,45 @@
      inkscape:groupmode="layer"
      id="layer1">
     <path
-       style="opacity:1;fill:blue;fill-opacity:1;stroke:black;stroke-width:2;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:0.75294119"
+       style="opacity:1;fill:#$color;fill-opacity:1;stroke:#$bgcolor;stroke-width:2;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:0.75294119"
        d="M 256,126 L 280,102 L 264,102 L 264,62 L 248,62 L 248,102 L 232,102 L 256,126 z "
        id="path1872"
        sodipodi:nodetypes="cccccccc" />
     <text
        xml:space="preserve"
-       style="font-size:56px;font-weight:bold;text-align:center;text-anchor:middle;opacity:1;fill:blue;fill-opacity:1;stroke:black;stroke-width:2;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:0.75294119"
-       x="253.97656"
+       style="font-size:56px;font-weight:bold;text-align:center;text-anchor:middle;opacity:1;fill:#$color;fill-opacity:1;stroke:#$bgcolor;stroke-width:2;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:0.75294118"
+       x="255.49414"
        y="49.957031"
        id="text1874"><tspan
          sodipodi:role="line"
          id="tspan1876"
-         x="253.97656"
-         y="49.957031">BLUE BASE</tspan></text>
+         x="255.49414"
+         y="49.957031">$text</tspan></text>
   </g>
 </svg>
+EOF
+	enlargex=$(($sprwidth - $width))
+	enlargey=$(($sprheight - $height))
+
+	
+
+	inkscape -z -w $sprwidth -h $sprheight \
+		-a "$((-$enlargex * $svgwidth / $width / 2)):0:$(($svgwidth + $enlargex * $svgwidth / $width / 2)):$(($svgheight + $enlargey * $svgheight / $height))" \
+		-e "$name.png" "$name.svg"
+	./makespr32 -o "$name.sp2" -proj 5 "$name.png" $(($sprwidth / 2)) $(($sprheight - 1)) 0
+}
+
+sprite bluebase           0000ff 000000 "BLUE BASE"
+sprite danger             ff0000 ffff00 "DANGER"
+sprite flagcarrier        ffff00 000000 "FLAG CARRIER"
+sprite helpme             ffff00 ff0000 "HELP ME"
+sprite here               00ff00 000000 "HERE"
+sprite key-dropped        00ffff 000000 "DROPPED KEY"
+sprite keycarrier-blue    0000ff 000000 "KEY CARRIER"
+sprite keycarrier-finish  00ffff 000000 "RUN HERE"
+sprite keycarrier-friend  00ff00 000000 "KEY CARRIER"
+sprite keycarrier-pink    ff00ff 000000 "KEY CARRIER"
+sprite keycarrier-red     ff0000 000000 "KEY CARRIER"
+sprite keycarrier-yellow  ffff00 000000 "KEY CARRIER"
+sprite redbase            ff0000 000000 "RED BASE"
+sprite waypoint           008080 000000 "WAYPOINT"
