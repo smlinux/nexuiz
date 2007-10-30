4e284d5fc7848c30c9b5416c64bdc68e838007b8
div0 <div0@f962a42d-fe04-0410-a3ab-8c8b0445ebaa>
2007-10-30 15:36:02 +0000

    added an empty line below the tab buttons

--- data/qcsrc/menu-div0test/nexuiz/dialog_settings.c
+++ data/qcsrc/menu-div0test/nexuiz/dialog_settings.c
@@ -5,7 +5,7 @@ CLASS(NexuizSettingsDialog) EXTENDS(NexuizDialog)
 	ATTRIB(NexuizSettingsDialog, title, string, "Settings")
 	ATTRIB(NexuizSettingsDialog, color, vector, '0.5 0.5 1')
 	ATTRIB(NexuizSettingsDialog, intendedWidth, float, 0.96)
-	ATTRIB(NexuizSettingsDialog, rows, float, 16)
+	ATTRIB(NexuizSettingsDialog, rows, float, 17)
 	ATTRIB(NexuizSettingsDialog, columns, float, 6)
 ENDCLASS(NexuizSettingsDialog)
 #endif
@@ -14,13 +14,14 @@ ENDCLASS(NexuizSettingsDialog)
 void fillNexuizSettingsDialog(entity me)
 {
 	entity mc;
-	mc = makeNexuizTabController(me.rows - 1);
+	mc = makeNexuizTabController(me.rows - 2);
 	me.TR(me);
 		me.TD(me, 1, 1, mc.makeTabButton(mc, "Input",   makeNexuizVideoSettingsTab()));
 		me.TD(me, 1, 1, mc.makeTabButton(mc, "Video",   makeNexuizVideoSettingsTab()));
 		me.TD(me, 1, 1, mc.makeTabButton(mc, "Effects", makeNexuizEffectsSettingsTab()));
 		me.TD(me, 1, 1, mc.makeTabButton(mc, "Audio",   makeNexuizVideoSettingsTab()));
 	me.TR(me);
-		me.TD(me, me.rows - 1, me.columns, mc);
+	me.TR(me);
+		me.TD(me, me.rows - 2, me.columns, mc);
 }
 #endif
