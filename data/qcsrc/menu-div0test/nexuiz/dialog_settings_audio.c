3202f5b235e9c34b431f3b959ae06db55c28c1d1
div0 <div0@f962a42d-fe04-0410-a3ab-8c8b0445ebaa>
2008-01-10 11:25:11 +0000

    force percent sliders to have 0 digits

--- data/qcsrc/menu-div0test/nexuiz/dialog_settings_audio.c
+++ data/qcsrc/menu-div0test/nexuiz/dialog_settings_audio.c
@@ -32,14 +32,17 @@ void fillNexuizAudioSettingsTab(entity me)
 		me.TD(me, 1, 1, e = makeNexuizTextLabel(0, "Music Volume:"));
 		me.TD(me, 1, 2, e = makeNexuizSlider(0, 1, 0.02, "bgmvolume"));
 			e.valueDisplayMultiplier = 100;
+			e.valueDigits = 0;
 	me.TR(me);
 		me.TD(me, 1, 1, e = makeNexuizTextLabel(0, "Game Volume:"));
 		me.TD(me, 1, 2, e = makeNexuizSlider(0, 1, 0.02, "volume"));
 			e.valueDisplayMultiplier = 100;
+			e.valueDigits = 0;
 	me.TR(me);
 		me.TD(me, 1, 1, e = makeNexuizTextLabel(0, "Ambient Volume:"));
 		me.TD(me, 1, 2, e = makeNexuizSlider(0, 1, 0.02, "snd_staticvolume"));
 			e.valueDisplayMultiplier = 100;
+			e.valueDigits = 0;
 
 	me.gotoXY(me, 0, 3.5); me.setFirstColumn(me, me.currentColumn);
 		me.TD(me, 1, 1, e = makeNexuizTextLabel(0, "Frequency:"));
