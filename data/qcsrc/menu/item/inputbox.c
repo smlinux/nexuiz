c736fdf83ee17a23304ffe4b63522d04e73d04fd
div0 <div0@f962a42d-fe04-0410-a3ab-8c8b0445ebaa>
2008-01-30 19:04:21 +0000

    make Shift- or Ctrl-Backspace no longer clear input fields...; only Ctrl-Del should clear the field

--- data/qcsrc/menu/item/inputbox.c
+++ data/qcsrc/menu/item/inputbox.c
@@ -106,17 +106,14 @@ float keyDownInputBox(entity me, float key, float ascii, float shift)
 			me.cursorPos = strlen(me.text);
 			return 1;
 		case K_BACKSPACE:
-			if(shift)
-				me.setText(me, "");
-			else
-				if(me.cursorPos > 0)
-				{
-					me.cursorPos -= 1;
-					me.setText(me, strcat(substring(me.text, 0, me.cursorPos), substring(me.text, me.cursorPos + 1, strlen(me.text) - me.cursorPos - 1)));
-				}
+			if(me.cursorPos > 0)
+			{
+				me.cursorPos -= 1;
+				me.setText(me, strcat(substring(me.text, 0, me.cursorPos), substring(me.text, me.cursorPos + 1, strlen(me.text) - me.cursorPos - 1)));
+			}
 			return 1;
 		case K_DEL:
-			if(shift)
+			if(shift & S_CTRL)
 				me.setText(me, "");
 			else
 				me.setText(me, strcat(substring(me.text, 0, me.cursorPos), substring(me.text, me.cursorPos + 1, strlen(me.text) - me.cursorPos - 1)));
