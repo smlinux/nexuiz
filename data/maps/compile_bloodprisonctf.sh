61d7fcfd0dcb182769c49682f7f2e8f71bc16c85
lordhavoc <lordhavoc@f962a42d-fe04-0410-a3ab-8c8b0445ebaa>
2008-02-14 20:13:53 +0000

    recompiled bloodprisonctf with -scale 1.2, this fixed the lightgrid

---
+++ data/maps/compile_bloodprisonctf.sh
@@ -0,0 +1,6 @@
+Q3MAP2="q3map2 -v -game quake3 -fs_basepath ../../ -fs_game data"
+MAP=bloodprisonctf.map
+$Q3MAP2 -meta -samplesize 8 -mv 1000000 -mi 6000000 $MAP
+$Q3MAP2 -scale 1.2 $MAP
+$Q3MAP2 -vis $MAP
+$Q3MAP2 -light -deluxe -fast -filter -deluxe -patchshadows -samples 3 $MAP
