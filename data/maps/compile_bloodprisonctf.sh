ff9b5f8183d859b20ee0e7630506e6732d3f359c
div0 <div0@f962a42d-fe04-0410-a3ab-8c8b0445ebaa>
2008-02-25 00:19:36 +0000

    winner dialog; bloodprisonctf now in the RIGHT size (it was not scaled before!)

--- data/maps/compile_bloodprisonctf.sh
+++ data/maps/compile_bloodprisonctf.sh
@@ -4,3 +4,5 @@ $Q3MAP2 -meta -samplesize 8 -mv 1000000 -mi 6000000 $MAP
 $Q3MAP2 -scale 1.2 $MAP
 $Q3MAP2 -vis $MAP
 $Q3MAP2 -light -deluxe -fast -filter -deluxe -patchshadows -samples 3 $MAP
+
+# nexuiz-map-compiler bloodprisonctf -scale 1.2 -bsp -samplesize 8 -light -deluxe -fast -filter -deluxe -patchshadows -samples 3
