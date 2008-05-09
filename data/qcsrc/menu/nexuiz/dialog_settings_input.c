bc9681a9e5f4bf1309b9f9dfe3aadf04193d221f
div0 <div0@f962a42d-fe04-0410-a3ab-8c8b0445ebaa>
2008-05-09 15:31:20 +0000

    menu_mouse_speed lower again

--- data/qcsrc/menu/nexuiz/dialog_settings_input.c
+++ data/qcsrc/menu/nexuiz/dialog_settings_input.c
@@ -47,7 +47,7 @@ void fillNexuizInputSettingsTab(entity me)
 		me.TD(me, 1, 2, e = makeNexuizSlider(1, 32, 0.2, "sensitivity"));
 	me.TR(me);
 		me.TD(me, 1, 1, e = makeNexuizTextLabel(0, "Menu mouse speed:"));
-		me.TD(me, 1, 2, e = makeNexuizSlider(0.25, 8.0, 0.05, "menu_mouse_speed"));
+		me.TD(me, 1, 2, e = makeNexuizSlider(0.2, 4.0, 0.1, "menu_mouse_speed"));
 	me.TR(me);
 		me.TD(me, 1, 3, e = makeNexuizCheckBox(0, "m_filter", "Mouse filter"));
 	me.TR(me);
