a1e68f0911a1df3403339f437243e3950afb187e
div0 <div0@f962a42d-fe04-0410-a3ab-8c8b0445ebaa>
2008-05-10 15:17:09 +0000

    20 -> 32 userbinds Menu mouse speed -> UI mouse speed (fits better)

--- data/qcsrc/menu/nexuiz/dialog_settings_input.c
+++ data/qcsrc/menu/nexuiz/dialog_settings_input.c
@@ -46,7 +46,7 @@ void fillNexuizInputSettingsTab(entity me)
 		me.TD(me, 1, 1, e = makeNexuizTextLabel(0, "Sensitivity:"));
 		me.TD(me, 1, 2, e = makeNexuizSlider(1, 32, 0.2, "sensitivity"));
 	me.TR(me);
-		me.TD(me, 1, 1, e = makeNexuizTextLabel(0, "Menu mouse speed:"));
+		me.TD(me, 1, 1, e = makeNexuizTextLabel(0, "UI mouse speed:"));
 		me.TD(me, 1, 2, e = makeNexuizSlider(0.2, 4.0, 0.1, "menu_mouse_speed"));
 	me.TR(me);
 		me.TD(me, 1, 3, e = makeNexuizCheckBox(0, "m_filter", "Mouse filter"));
