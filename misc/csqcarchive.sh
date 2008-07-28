0059eea5908b6715ce64e398a1e4674cc8a4ae37
div0 <div0@f962a42d-fe04-0410-a3ab-8c8b0445ebaa>
2008-07-28 06:02:33 +0000

    fix...

--- misc/csqcarchive.sh
+++ misc/csqcarchive.sh
@@ -4,6 +4,7 @@ set -e
 
 make crc16
 crc16=`pwd`/crc16
+out=`pwd`/csqcarchive.zip
 
 t=`mktemp -dt csqcarchive.XXXXXX`
 cd "$t"
@@ -28,12 +29,13 @@ revs()
 	} | sort -n
 }
 
-revs | less
-
+rm -f "$out"
 for rev in `revs`; do
 	svn checkout -r"$rev" svn://svn.icculus.org/nexuiz/trunk/data/qcsrc
 	rm -f Makefile csprogs.dat
 	wget -OMakefile "http://svn.icculus.org/*checkout*/nexuiz/trunk/data/Makefile?revision=$rev" || continue
 	make csprogs.dat || continue
-	mv csprogs.dat csprogs.dat.`$crc16 < csprogs.dat`
+	nm="csprogs.dat.`$crc16 < csprogs.dat`"
+	mv csprogs.dat "$nm"
+	zip -9r "$out" "$nm"
 done
