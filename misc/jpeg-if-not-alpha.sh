87b42a2b4a9f2eef82d5b5ac850d77cccc9f183c
div0 <div0@f962a42d-fe04-0410-a3ab-8c8b0445ebaa>
2008-12-28 21:10:25 +0000

    new change list

--- misc/jpeg-if-not-alpha.sh
+++ misc/jpeg-if-not-alpha.sh
@@ -8,13 +8,13 @@ for X in "$@"; do
 	case "$X" in
 		*.jpg)
 			if [ -n "$scaledown" ]; then
-				mogrify "$X" -geometry "$scaledown<" -quality 100
+				mogrify -geometry "$scaledown>" -quality 100 "$X"
 			fi
 			jpegoptim --strip-all -m$qual "$X"
 			;;
 		*.png|*.tga)
 			if [ -n "$scaledown" ]; then
-				mogrify "$X" -geometry "$scaledown<" -quality 100
+				mogrify -geometry "$scaledown>" -quality 100 "$X"
 			fi
 			if convert "$X" -depth 16 RGBA:- | perl -e 'while(read STDIN, $_, 8) { substr($_, 6, 2) eq "\xFF\xFF" or exit 1; ++$pix; } exit not $pix;'; then
 				echo "$X has no alpha, converting"
