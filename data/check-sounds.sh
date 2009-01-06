8813eee115a88580b325a3b147ef02bb66c3eff3
div0 <div0@f962a42d-fe04-0410-a3ab-8c8b0445ebaa>
2009-01-06 12:37:03 +0000

    sounds...

--- data/check-sounds.sh
+++ data/check-sounds.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+allidentifiers=`grep "^		_VOICEMSG" qcsrc/server/defs.qh | sed "s/.*(//; s/).*//;"`
+
 allsounds=`find sound -name .svn -prune -o \( -name \*.ogg -o -name \*.wav \) -print`
 for S in $allsounds; do
 	SND=${S#sound/}
@@ -77,45 +79,76 @@ psound()
 
 for S in models/player/*.sounds sound/player/default.sounds; do
 	if [ "$S" = "sound/player/default.sounds" ] || [ -f "${S#.sounds}" ]; then
-		while read -r TITLE SOUND COUNT; do
-			case "$TITLE" in
-				//*)
-					good=false
-					case "$COUNT" in
-						0)
-							if psoundtry "$SOUND"; then
-								good=false
-							fi
-							;;
-						*)
-							for i in `seq 1 $COUNT`; do
-								if psoundtry "$SOUND$i"; then
-									good=true
+		{
+			identifiers_seen=
+			while read -r TITLE SOUND COUNT; do
+				case "$TITLE" in
+					//TAG*)
+						;;
+					//*)
+						identifiers_seen="$identifiers_seen ${TITLE#//}"
+						good=false
+						case "$COUNT" in
+							0)
+								if psoundtry "$SOUND"; then
+									good=false
 								fi
-							done
-							;;
-					esac
-					if $good; then
-						echo "$S references existing sound $SOUND but commented out"
-					fi
-					;;
-				*[!a-z0-9]*)
-					echo "Unknown identifier $TITLE in $S"
-					;;
-				*)
-					case "$COUNT" in
-						0)
-							psound "$SOUND"
-							;;
-						*)
-							for i in `seq 1 $COUNT`; do
-								psound "$SOUND$i"
-							done
-							;;
-					esac
-					;;
-			esac
-		done < "$S"
+								;;
+							*)
+								for i in `seq 1 $COUNT`; do
+									if psoundtry "$SOUND$i"; then
+										good=true
+									fi
+								done
+								;;
+						esac
+						if $good; then
+							echo "$S references existing sound $SOUND but commented out"
+						else
+							echo "$S does not have a sound for ${TITLE#//} yet"
+						fi
+						;;
+					*)
+						identifiers_seen="$identifiers_seen $TITLE"
+						case "$COUNT" in
+							0)
+								psound "$SOUND"
+								;;
+							*)
+								for i in `seq 1 $COUNT`; do
+									psound "$SOUND$i"
+								done
+								;;
+						esac
+
+						;;
+				esac
+			done
+			missing=`
+				{
+					for X in $identifiers_seen; do
+						echo "$X"
+						echo "$X"
+					done
+					for X in $allidentifiers; do
+						echo "$X"
+					done
+				} | sort | uniq -u
+			`
+			invalid=`
+				{
+					for X in $identifiers_seen; do
+						echo "$X"
+					done
+					for X in $allidentifiers; do
+						echo "$X"
+						echo "$X"
+					done
+				} | sort | uniq -u
+			`
+			[ -z "$invalid" ] || echo "$S specifies invalid sound identifiers `echo $invalid`"
+			[ -z "$missing" ] || echo "$S lacks sound identifiers `echo $missing`"
+		} < "$S"
 	else
 		echo "$S exists for nonexisting player model"
 	fi
