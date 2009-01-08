edc8c34f5a73a1355bb1f6bf92848a5a0d87cc84
div0 <div0@f962a42d-fe04-0410-a3ab-8c8b0445ebaa>
2009-01-08 13:02:10 +0000

    cvar cl_taunt_directional

--- data/qcsrc/menu/nexuiz/dialog_settings_audio.c
+++ data/qcsrc/menu/nexuiz/dialog_settings_audio.c
@@ -131,9 +131,11 @@ void fillNexuizAudioSettingsTab(entity me)
 		me.TD(me, 1, 1, e = makeNexuizSliderCheckBox(0, 1, sl, "Automatic taunts"));
 		if(sl.value != e.savedValue)
 			e.savedValue = 0.65; // default
-		me.TR(me);
+	me.TR(me);
 		me.TD(me, 1, 3, e = makeNexuizTextLabel(0.1, "Frequency:"));
 		me.TD(me, 1, 2, sl);
+	me.TR(me);
+		me.TD(me, 1, 3, e = makeNexuizCheckBox(0, "cl_taunt_directional", "Play taunts directionally"));
 	me.TR(me);
 	me.TR(me);
 		me.TD(me, 1, 2.8, e = makeNexuizCheckBox(0, "cl_hitsound", "Hit indicator"));
