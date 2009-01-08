49410a71e25969c44d4313648e4988d56dc06046
esteel <esteel@f962a42d-fe04-0410-a3ab-8c8b0445ebaa>
2009-01-08 19:03:10 +0000

    spam spam spam

--- data/qcsrc/menu/nexuiz/playerlist.c
+++ data/qcsrc/menu/nexuiz/playerlist.c
@@ -108,16 +108,16 @@ void drawListBoxItemNexuizPlayerList(entity me, float i, vector absSize, float i
 	
 	s = me.getPlayerList(me, i, PLAYERPARM_NAME);
 
-	print(s, "\n");
+	dprint(s, "\n");
 
 	if(rgb != '1 1 1')
 		s = strdecolorize(s);
 
-	print(s, "\n");
+	dprint(s, "\n");
 
 	s = draw_TextShortenToWidth(s, me.columnNameSize / me.realFontSize_x, rgb == '1 1 1');
 
-	print(s, "\n");
+	dprint(s, "\n");
 
 	draw_Text(me.realUpperMargin2 * eY + (me.columnNameOrigin + 0.00 * (me.columnNameSize - draw_TextWidth(s, 1) * me.realFontSize_x)) * eX, s, me.realFontSize, rgb, 1, rgb == '1 1 1');
 }
