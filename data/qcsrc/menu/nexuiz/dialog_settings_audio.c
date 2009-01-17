c6f3a7c84f7e204641ffd52f19e1789f6a2946f6
div0 <div0@f962a42d-fe04-0410-a3ab-8c8b0445ebaa>
2009-01-17 17:53:48 +0000

    moved gibbing from server to client
    SavageX: please reimplement sv_gentle as client cvar in client/gibs.qc

--- data/qcsrc/menu/nexuiz/dialog_settings_audio.c
+++ data/qcsrc/menu/nexuiz/dialog_settings_audio.c
@@ -56,7 +56,7 @@ void fillNexuizAudioSettingsTab(entity me)
 		setDependentStringNotEqual(s, "volume", "0");
 	me.TR(me);
 		me.TDempty(me, 0.2);
-		s = makeNexuizDecibelsSlider(-20, 0, 0.5, "snd_playerchannel6volume");
+		s = makeNexuizDecibelsSlider(-20, 0, 0.5, "snd_playerchannel6volume snd_csqcchannel6volume");
 		me.TD(me, 1, 0.8, e = makeNexuizSliderCheckBox(-1000000, 1, s, "Pain:"));
 		me.TD(me, 1, 2, s);
 		setDependentStringNotEqual(e, "volume", "0");
@@ -72,7 +72,7 @@ void fillNexuizAudioSettingsTab(entity me)
 	me.TR(me);
 		me.TDempty(me, 0.2);
 		s = makeNexuizDecibelsSlider(-20, 0, 0.5, "snd_entchannel4volume");
-		makeMulti(s, "snd_playerchannel4volume snd_entchannel6volume snd_csqcchannel6volume");
+		makeMulti(s, "snd_playerchannel4volume snd_entchannel6volume snd_csqcchannel4volume");
 		me.TD(me, 1, 0.8, e = makeNexuizSliderCheckBox(-1000000, 1, s, "Shots:"));
 		me.TD(me, 1, 2, s);
 		setDependentStringNotEqual(e, "volume", "0");
