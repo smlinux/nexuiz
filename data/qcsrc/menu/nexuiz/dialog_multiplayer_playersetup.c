9c01beb893c356200a1e5b7c254011e35149d093
esteel <esteel@f962a42d-fe04-0410-a3ab-8c8b0445ebaa>
2009-03-03 17:39:59 +0000

    use part of terencehills patch and moved the skin colors to the right in the playersetup dialog.  seems to fit the overall structure better

--- data/qcsrc/menu/nexuiz/dialog_multiplayer_playersetup.c
+++ data/qcsrc/menu/nexuiz/dialog_multiplayer_playersetup.c
@@ -47,23 +47,33 @@ void fillNexuizPlayerSettingsTab(entity me)
 	me.TR(me);
 	me.TR(me);
 	me.TR(me);
-		me.TD(me, 1, 1, e = makeNexuizTextLabel(0, "First Color:"));
-		n = 16 - !cvar("developer");
-		for(i = 0; i < n; ++i)
-			me.TDNoMargin(me, 1, 2 / n, e = makeNexuizColorButton(1, 0, i), '1 0 0');
-	me.TR(me);
-		me.TD(me, 1, 1, e = makeNexuizTextLabel(0, "Second Color:"));
-		for(i = 0; i < n; ++i)
-			me.TDNoMargin(me, 1, 2 / n, e = makeNexuizColorButton(2, 1, i), '1 0 0');
 	me.TR(me);
 		pms = makeNexuizPlayerModelSelector();
+		me.TDempty(me, 0.5);
 		me.TD(me, 1, 0.3, e = makeNexuizButton("<<", '0 0 0'));
 			e.onClick = PlayerModelSelector_Prev_Click;
 			e.onClickEntity = pms;
-		me.TD(me, me.rows - me.currentRow - 1, 2.4, pms);
+		me.TD(me, me.rows - me.currentRow - 1, 1.9, pms);
 		me.TD(me, 1, 0.3, e = makeNexuizButton(">>", '0 0 0'));
 			e.onClick = PlayerModelSelector_Next_Click;
 			e.onClickEntity = pms;
+	me.gotoRC(me, 8, 0.1);
+		me.TD(me, 1, 0.6, e = makeNexuizTextLabel(1, "Skin colors"));
+	me.gotoRC(me, 9, 0.2); me.setFirstColumn(me, me.currentColumn);
+		n = 16 - !cvar("developer");
+		for(i = 0; i < n; ++i)
+		{
+			me.gotoRC(me, 8 + i * 0.75, 0.1);
+			me.TR(me);
+				me.TDNoMargin(me, 1, (2 / n) + 0.1, e = makeNexuizColorButton(1, 0, i), '1 0 0');
+		}
+	me.gotoRC(me, 9, 0.45); me.setFirstColumn(me, me.currentColumn);
+		for(i = 0; i < n; ++i)
+		{
+			me.gotoRC(me, 8 + i * 0.75, 0.4);
+			me.TR(me);
+				me.TDNoMargin(me, 1, (2 / n) + 0.1, e = makeNexuizColorButton(2, 1, i), '1 0 0');
+		}
 
 	me.gotoRC(me, 0, 3.5); me.setFirstColumn(me, me.currentColumn);
 		me.TD(me, 1, 1, e = makeNexuizTextLabel(0, "Field of View:"));
