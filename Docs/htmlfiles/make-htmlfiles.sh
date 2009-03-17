089d49ec9672e84f3f1f93befaf2c453d6265c64
div0 <div0@f962a42d-fe04-0410-a3ab-8c8b0445ebaa>
2009-03-17 14:09:16 +0000

    add weapon images to the docs

---
+++ Docs/htmlfiles/make-htmlfiles.sh
@@ -0,0 +1,4 @@
+for X in *.aft; do
+	aft "$X"
+	{ cat header.html-part; perl -ne 'print unless 1 .. /<body>/ or /<\/body>/ .. 0' "${X%.aft}.html"; cat footer.html-part; } > "../${X%.aft}.html"
+done
