a852f3b3a33cba97bda2c11338bd837b510e2f7a
div0 <div0@f962a42d-fe04-0410-a3ab-8c8b0445ebaa>
2009-04-11 19:30:33 +0000

    make the sh and bat files able to work from their directory, if server.cfg is set up

--- server/server_havoc_linux.sh
+++ server/server_havoc_linux.sh
@@ -6,4 +6,18 @@ case "$(uname -m)" in
 esac
 
 cd "`dirname "${0}"`"
+
+if ! [ -x "$executable" ]; then
+	if [ -x "../$executable" ] && { [ -f ~/.nexuiz/data/server.cfg ] || [ -f ../data/server.cfg ] || [ -f ~/.nexuiz/havoc/server.cfg ] || [ -f ../havoc/server.cfg ]; }; then
+		cd ..
+	else
+		echo "This script is not properly set up yet."
+		echo "Please refer to the instructions in readme.txt."
+		echo "In short:"
+		echo "- copy server.cfg to the data directory and adjust its settings"
+		echo "- move this file to the main directory of your Nexuiz installation"
+		exit 1
+	fi
+fi
+
 exec ./${executable} -game havoc +serverconfig server.cfg "${@}"
