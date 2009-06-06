e13be9473274d0016fcb53d0e376e87fb321fd54
div0 <div0@f962a42d-fe04-0410-a3ab-8c8b0445ebaa>
2009-06-06 09:56:39 +0000

    more files, more files

--- data/build-compat-pack.sh
+++ data/build-compat-pack.sh
@@ -4,45 +4,6 @@
 
 COMPAT_FILES="
 	effectinfo.txt
-	sound/announcer/male/amazing.ogg
-	sound/announcer/male/awesome.ogg
-	sound/misc/shield_respawn.wav
-	sound/misc/strength_respawn.wav
-	sound/player/pyria-skadi/coms/needhelp2.ogg
-	sound/weapons/unavailable.wav
-	sound/ctf/blue_dropped.wav
-	sound/ctf/red_dropped.wav
-	sound/ctf/flag_respawn.wav
-	sound/ctf/blue_taken.wav
-	sound/ctf/blue_capture.wav
-	sound/ctf/red_taken.wav
-	sound/ctf/red_capture.wav
-	sound/ctf/blue_returned.wav
-	sound/ctf/red_returned.wav
-	sound/misc/itemrespawncountdown.ogg
-	sound/misc/armor25.wav
-	sound/misc/powerup.ogg
-	sound/misc/armor17_5.wav
-	sound/misc/armor10.wav
-	sound/misc/armor1.wav
-	sound/misc/poweroff.wav
-	scripts/onslaught.shader
-	models/sprites/item-extralife_frame0.tga
-	models/sprites/item-extralife_frame1.tga
-	models/sprites/item-fuelregen_frame0.tga
-	models/sprites/item-fuelregen_frame1.tga
-	models/sprites/item-invis_frame0.tga
-	models/sprites/item-invis_frame1.tga
-	models/sprites/item-jetpack_frame0.tga
-	models/sprites/item-jetpack_frame1.tga
-	models/sprites/item-shield_frame0.tga
-	models/sprites/item-shield_frame1.tga
-	models/sprites/item-speed_frame0.tga
-	models/sprites/item-speed_frame1.tga
-	models/sprites/item-strength_frame0.tga
-	models/sprites/item-strength_frame1.tga
-	sound/weapons/weaponpickup.ogg
-	particles/particlefont.tga
 	models/gibs/arm.md3
 	models/gibs/arm.md3_0.skin
 	models/gibs/arm.md3_1.skin
@@ -69,6 +30,85 @@ COMPAT_FILES="
 	models/gibs/smallchest.md3_0.skin
 	models/gibs/smallchest.md3_1.skin
 	models/gibs/smallchest.md3_2.skin
+	models/onslaught/boom.md3
+	models/onslaught/controlpoint_icon_dmg1.md3
+	models/onslaught/controlpoint_icon_dmg2.md3
+	models/onslaught/controlpoint_icon_dmg3.md3
+	models/onslaught/controlpoint_icon_gib1.md3
+	models/onslaught/controlpoint_icon_gib2.md3
+	models/onslaught/controlpoint_icon_gib4.md3
+	models/onslaught/controlpoint_pad2.md3
+	models/onslaught/generator_dead.md3
+	models/onslaught/generator_dmg1.md3
+	models/onslaught/generator_dmg2.md3
+	models/onslaught/generator_dmg3.md3
+	models/onslaught/generator_dmg4.md3
+	models/onslaught/generator_dmg5.md3
+	models/onslaught/generator_dmg6.md3
+	models/onslaught/generator_dmg7.md3
+	models/onslaught/generator_dmg8.md3
+	models/onslaught/generator_dmg9.md3
+	models/onslaught/gen_gib1.md3
+	models/onslaught/gen_gib2.md3
+	models/onslaught/gen_gib3.md3
+	models/onslaught/ons_ray.md3
+	models/onslaught/shockwave.md3
+	models/onslaught/shockwavetransring.md3
+	models/sprites/item-extralife_frame0.tga
+	models/sprites/item-extralife_frame1.tga
+	models/sprites/item-fuelregen_frame0.tga
+	models/sprites/item-fuelregen_frame1.tga
+	models/sprites/item-invis_frame0.tga
+	models/sprites/item-invis_frame1.tga
+	models/sprites/item-jetpack_frame0.tga
+	models/sprites/item-jetpack_frame1.tga
+	models/sprites/item-shield_frame0.tga
+	models/sprites/item-shield_frame1.tga
+	models/sprites/item-speed_frame0.tga
+	models/sprites/item-speed_frame1.tga
+	models/sprites/item-strength_frame0.tga
+	models/sprites/item-strength_frame1.tga
+	particles/particlefont.tga
+	scripts/onslaught.shader
+	sound/announcer/male/amazing.ogg
+	sound/announcer/male/awesome.ogg
+	sound/ctf/blue_capture.wav
+	sound/ctf/blue_dropped.wav
+	sound/ctf/blue_returned.wav
+	sound/ctf/blue_taken.wav
+	sound/ctf/flag_respawn.wav
+	sound/ctf/red_capture.wav
+	sound/ctf/red_dropped.wav
+	sound/ctf/red_returned.wav
+	sound/ctf/red_taken.wav
+	sound/misc/armor10.wav
+	sound/misc/armor17_5.wav
+	sound/misc/armor1.wav
+	sound/misc/armor25.wav
+	sound/misc/itemrespawncountdown.ogg
+	sound/misc/poweroff.wav
+	sound/misc/powerup.ogg
+	sound/misc/shield_respawn.wav
+	sound/misc/strength_respawn.wav
+	sound/onslaught/electricity_explode.ogg
+	sound/onslaught/ons_hit1.ogg
+	sound/onslaught/ons_hit2.ogg
+	sound/onslaught/ons_spark1.ogg
+	sound/onslaught/ons_spark2.ogg
+	sound/onslaught/shockwave.ogg
+	sound/player/pyria-skadi/coms/needhelp2.ogg
+	sound/weapons/unavailable.wav
+	sound/weapons/weaponpickup.ogg
+	textures/bloodyskull_alien_glow.tga
+	textures/bloodyskull_alien.tga
+	textures/bloodyskull.jpg
+	textures/bloodyskull_robot_gloss.tga
+	textures/bloodyskull_robot_glow.tga
+	textures/bloodyskull_robot.tga
+	textures/generator_destroyed.tga
+	textures/generator_lightning2.tga
+	textures/generator_lightning.tga
+	textures/generator.tga
 	textures/meat_alien_gloss.tga
 	textures/meat_alien_glow.tga
 	textures/meat_alien_norm.tga
@@ -80,12 +120,16 @@ COMPAT_FILES="
 	textures/meat_robot_norm.tga
 	textures/meat_robot.tga
 	textures/meat.tga
-	textures/bloodyskull_alien_glow.tga
-	textures/bloodyskull_alien.tga
-	textures/bloodyskull.jpg
-	textures/bloodyskull_robot_gloss.tga
-	textures/bloodyskull_robot_glow.tga
-	textures/bloodyskull_robot.tga
+	textures/ons_boom1.tga
+	textures/ons_gengib.tga
+	textures/ons_icon.tga
+	textures/ons_icon_thrust.tga
+	textures/ons_pad.tga
+	textures/ons_ray.tga
+	textures/ons_shockwave1.tga
+	textures/ons_shockwave2.tga
+	textures/ons_smoke1.tga
+	textures/ons_text.tga
 "
 
 rm -rf pack
