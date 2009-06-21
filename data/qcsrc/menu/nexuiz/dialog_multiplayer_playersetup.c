131bfc041304eb17f5c69b4397c04a3cb1b9cbd9
div0 <div0@f962a42d-fe04-0410-a3ab-8c8b0445ebaa>
2009-06-21 10:44:11 +0000

    add new options

--- data/qcsrc/menu/nexuiz/dialog_multiplayer_playersetup.c
+++ data/qcsrc/menu/nexuiz/dialog_multiplayer_playersetup.c
@@ -146,8 +146,13 @@ void fillNexuizPlayerSettingsTab(entity me)
 			e.onClickEntity = main.radarDialog;
 		me.TDempty(me, 0.5);
 	me.TR(me);
+	me.TR(me);
+		me.TD(me, 1, 1, e = makeNexuizTextLabel(0, "Force Models:"));
+		me.TD(me, 1, 2/3, e = makeNexuizRadioButton(1, string_null, string_null, "None"));
+		me.TD(me, 1, 2/3, e = makeNexuizRadioButton(1, "cl_forceplayermodelsfromnexuiz", string_null, "Custom"));
+		me.TD(me, 1, 2/3, e = makeNexuizRadioButton(1, "cl_forceplayermodels", string_null, "All"));
 
 	me.gotoRC(me, me.rows - 1, 0);
-		me.TD(me, 1, me.columns, makeNexuizCommandButton("Apply immediately", '0 0 0', "color -1 -1;name \"$_cl_name\";sendcvar cl_weaponpriority;sendcvar cl_zoomfactor;sendcvar cl_zoomspeed;sendcvar cl_autoswitch;sendcvar cl_shownames", COMMANDBUTTON_APPLY));
+		me.TD(me, 1, me.columns, makeNexuizCommandButton("Apply immediately", '0 0 0', "color -1 -1;name \"$_cl_name\";sendcvar cl_weaponpriority;sendcvar cl_zoomfactor;sendcvar cl_zoomspeed;sendcvar cl_autoswitch;sendcvar cl_shownames;sendcvar cl_forceplayermodelsfromnexuiz;sendcvar cl_forceplayermodels", COMMANDBUTTON_APPLY));
 }
 #endif
