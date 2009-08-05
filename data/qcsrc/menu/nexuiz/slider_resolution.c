306faab8dcdc154024ca9c68a9b24d4b1ba8ea22
div0 <div0@f962a42d-fe04-0410-a3ab-8c8b0445ebaa>
2009-08-05 13:29:56 +0000

    show only available resolutions

--- data/qcsrc/menu/nexuiz/slider_resolution.c
+++ data/qcsrc/menu/nexuiz/slider_resolution.c
@@ -18,28 +18,47 @@ entity makeNexuizResolutionSlider()
 }
 void addResolutionNexuizResolutionSlider(entity me, float w, float h, float cw, float ch)
 {
+	float f;
+	f = max(cw / w, ch / h);
+	if(f > 1)
+	{
+		cw = rint(cw / f);
+		ch = rint(ch / f);
+	}
 	me.addValue(me, strzone(strcat(ftos(w), "x", ftos(h))), strzone(strcat(ftos(w), " ", ftos(h), " ", ftos(cw), " ", ftos(ch))));
 	// FIXME (in case you ever want to dynamically instantiate this): THIS IS NEVER FREED
 }
 void configureNexuizResolutionSliderNexuizResolutionSlider(entity me)
 {
+	float i;
+	vector r0, r, c;
+
 	me.configureNexuizTextSlider(me, "vid_width");
-	me.addResolution(me, 640, 480, 640, 480);
-	me.addResolution(me, 800, 480, 800, 480);
-	me.addResolution(me, 800, 600, 800, 600);
-	me.addResolution(me, 1024, 600, 1024, 600);
-	me.addResolution(me, 1024, 768, 800, 600);
-	me.addResolution(me, 1152, 864, 800, 600);
-	me.addResolution(me, 1280, 800, 960, 600);
-	me.addResolution(me, 1280, 960, 800, 600);
-	me.addResolution(me, 1280, 1024, 800, 640);
-	me.addResolution(me, 1440, 900, 960, 600);
-	me.addResolution(me, 1600, 900, 960, 540);
-	me.addResolution(me, 1600, 1200, 800, 600);
-	me.addResolution(me, 1680, 1050, 960, 600);
-	me.addResolution(me, 1920, 1080, 960, 540);
-	me.addResolution(me, 1920, 1200, 960, 600);
-	me.addResolution(me, 2048, 1536, 800, 600);
+
+	c = '0 0 0';
+	r0 = '0 0 0';
+	for(i = 0;; ++i)
+	{
+		r = getresolution(i);
+		if(r_x == 0 && r_y == 0)
+			break;
+		if(r == r0)
+			continue;
+		r0 = r;
+		if(r_x < 640 || r_y < 400)
+			continue;
+		c_x = 800;
+		c_y = rint(c_x * r_y / r_x);
+		if(c_y >= 600)
+		{
+			me.addResolution(me, r_x, r_y, c_x, c_y);
+			continue;
+		}
+		c_y = 600;
+		c_x = rint(c_y * r_x / r_y);
+		me.addResolution(me, r_x, r_y, c_x, c_y);
+	}
+
 	me.configureNexuizTextSliderValues(me);
 }
 void loadCvarsNexuizResolutionSlider(entity me)
