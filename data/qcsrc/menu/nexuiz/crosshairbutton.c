8de8993da55509e9ab425ed700a2315d5380480b
div0 <div0@f962a42d-fe04-0410-a3ab-8c8b0445ebaa>
2009-08-20 07:06:20 +0000

    high: picmip 1 menu: REALLY fix crosshair button

--- data/qcsrc/menu/nexuiz/crosshairbutton.c
+++ data/qcsrc/menu/nexuiz/crosshairbutton.c
@@ -7,6 +7,7 @@ CLASS(NexuizCrosshairButton) EXTENDS(RadioButton)
 	ATTRIB(NexuizCrosshairButton, image, string, SKINGFX_CROSSHAIRBUTTON)
 
 	ATTRIB(NexuizCrosshairButton, useDownAsChecked, float, 1)
+	ATTRIB(NexuizCrosshairButton, src3, string, string_null)
 
 	ATTRIB(NexuizCrosshairButton, cvarName, string, string_null)
 	ATTRIB(NexuizCrosshairButton, cvarValueFloat, float, 0)
@@ -31,7 +32,7 @@ void configureNexuizCrosshairButtonNexuizCrosshairButton(entity me, float theGro
 	me.loadCvars(me);
 	me.configureRadioButton(me, string_null, me.fontSize, me.image, theGroup, 0);
 	me.srcMulti = 1;
-	me.src2 = strzone(strcat("/gfx/crosshair", ftos(me.cvarValueFloat)));
+	me.src3 = strzone(strcat("/gfx/crosshair", ftos(me.cvarValueFloat)));
 }
 void setCheckedNexuizCrosshairButton(entity me, float val)
 {
@@ -59,14 +60,28 @@ void saveCvarsNexuizCrosshairButton(entity me)
 }
 void drawNexuizCrosshairButton(entity me)
 {
-	me.color2 = eX * cvar("crosshair_color_red") + eY * cvar("crosshair_color_green") + eZ * cvar("crosshair_color_blue");
-	me.alpha2 = cvar("crosshair_color_alpha");
-	me.src2scale = min((draw_PictureSize(me.src2) * eY * cvar("crosshair_size")) / me.size_y, (draw_PictureSize(me.src2) * eX * cvar("crosshair_size")) / me.size_x, 0.8);
+	vector sz, rgb;
+	float a;
+
+	rgb = eX * cvar("crosshair_color_red") + eY * cvar("crosshair_color_green") + eZ * cvar("crosshair_color_blue");
+	a = cvar("crosshair_color_alpha");
+
 	if(!me.checked && !me.focused)
 	{
-		me.alpha2 *= me.disabledAlpha;
-		me.color2 = '1 1 1';
+		a *= me.disabledAlpha;
+		rgb = '1 1 1';
 	}
+
 	drawCheckBox(me);
+
+	sz = draw_PictureSize(me.src3);
+	sz = globalToBoxSize(sz, draw_scale);
+	sz = sz * cvar("crosshair_size");
+	if(sz_x > 0.95)
+		sz = sz * (0.95 / sz_x);
+	if(sz_y > 0.95)
+		sz = sz * (0.95 / sz_y);
+
+	draw_Picture('0.5 0.5 0' - 0.5 * sz, me.src3, sz, rgb, a);
 }
 #endif
