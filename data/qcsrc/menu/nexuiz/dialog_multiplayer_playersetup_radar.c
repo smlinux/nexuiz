c3818181f9e83f90d476970a8ed8cc690080cdb7
fruitiex <fruitiex@f962a42d-fe04-0410-a3ab-8c8b0445ebaa>
2009-09-13 19:01:51 +0000

    g_waypoinstprite_alpha cvar

--- data/qcsrc/menu/nexuiz/dialog_multiplayer_playersetup_radar.c
+++ data/qcsrc/menu/nexuiz/dialog_multiplayer_playersetup_radar.c
@@ -93,6 +93,11 @@ void fillNexuizRadarDialog(entity me)
 			me.TD(me, 1, 3, e = makeNexuizSlider(0.5, 1.5, 0.01, "g_waypointsprite_scale"));
 				setDependent(e, "cl_hidewaypoints", 0, 0);
 	me.TR(me);
+ 		me.TDempty(me, 0.2);
+		me.TD(me, 1, 0.8, e = makeNexuizTextLabel(0, "Waypoint alpha:"));
+			me.TD(me, 1, 3, e = makeNexuizSlider(0.1, 1, 0.01, "g_waypointsprite_alpha"));
+				setDependent(e, "cl_hidewaypoints", 0, 0);
+	me.TR(me);
 		me.TDempty(me, 0.2);
 		me.TD(me, 1, 0.8, e = makeNexuizTextLabel(0, "Show names:"));
 		me.TD(me, 1, 3, e = makeNexuizTextSlider("cl_shownames"));
