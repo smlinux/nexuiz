f82072b5624204b0d8555a820b8e787ee276ce12
div0 <div0@f962a42d-fe04-0410-a3ab-8c8b0445ebaa>
2009-09-21 12:36:52 +0000

    fix one more place wiht color code

--- server/rcon2irc/rcon2irc.pl
+++ server/rcon2irc/rcon2irc.pl
@@ -1538,7 +1538,7 @@ sub cond($)
 				my $sep = ' ';
 				for(@t)
 				{
-					$scores_string .= $sep . sprintf "\003%02d\%d\017", $color_team2irc_table{$_->{team}}, $_->{score};
+					$scores_string .= $sep . "\003" . $color_team2irc_table{$_->{team}}. "\002\002" . $_->{score} . "\017";
 					$sep = ':';
 				}
 			}
