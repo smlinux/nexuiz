2224de8b8a0f82c86c04e97208032f4c4131fe69
div0 <div0@f962a42d-fe04-0410-a3ab-8c8b0445ebaa>
2009-09-24 05:49:56 +0000

    change weapons*.cfg to balance*.cfg, allow "exec default25.cfg" and "exec default26.cfg" to switch between the settings without too much harm

--- data/update-cvarcount.sh
+++ data/update-cvarcount.sh
@@ -1,27 +1,27 @@
 #!/bin/sh
 
 countd=`awk '/^seta? g_/ { print $2; }' defaultNexuiz.cfg | sort -u | tr -d '\r' | md5sum | cut -c 1-32`
-countw=`awk '/^seta? g_/ { print $2; }' weapons.cfg       | sort -u | tr -d '\r' | md5sum | cut -c 1-32`
-counth=`awk '/^seta? g_/ { print $2; }' weaponsHavoc.cfg  | sort -u | tr -d '\r' | md5sum | cut -c 1-32`
-countl=`awk '/^seta? g_/ { print $2; }' weapons25.cfg     | sort -u | tr -d '\r' | md5sum | cut -c 1-32`
+countw=`awk '/^seta? g_/ { print $2; }' balance.cfg       | sort -u | tr -d '\r' | md5sum | cut -c 1-32`
+counth=`awk '/^seta? g_/ { print $2; }' balanceHavoc.cfg  | sort -u | tr -d '\r' | md5sum | cut -c 1-32`
+countl=`awk '/^seta? g_/ { print $2; }' balance25.cfg     | sort -u | tr -d '\r' | md5sum | cut -c 1-32`
 
 if [ "$countw" != "$counth" ]; then
-	echo "Mismatch between weapons.cfg and weaponsHavoc.cfg. Aborting."
+	echo "Mismatch between balance.cfg and balanceHavoc.cfg. Aborting."
 	exit 1
 fi
 if [ "$countw" != "$countl" ]; then
-	echo "Mismatch between weapons.cfg and weaponsHavoc.cfg. Aborting."
+	echo "Mismatch between balance.cfg and balanceHavoc.cfg. Aborting."
 	exit 1
 fi
 
 sed -i -e "s/^set cvar_check_default .*/set cvar_check_default $countd/" defaultNexuiz.cfg
-sed -i -e "s/^set cvar_check_weapons .*/set cvar_check_weapons $countw/" weapons.cfg
-sed -i -e "s/^set cvar_check_weapons .*/set cvar_check_weapons $countw/" weaponsHavoc.cfg
-sed -i -e "s/^set cvar_check_weapons .*/set cvar_check_weapons $countw/" weapons25.cfg
+sed -i -e "s/^set cvar_check_balance .*/set cvar_check_balance $countw/" balance.cfg
+sed -i -e "s/^set cvar_check_balance .*/set cvar_check_balance $countw/" balanceHavoc.cfg
+sed -i -e "s/^set cvar_check_balance .*/set cvar_check_balance $countw/" balance25.cfg
 
 sed -e "
 	s/^string CVAR_CHECK_DEFAULT = .*/string CVAR_CHECK_DEFAULT = \"$countd\";/;
-	s/^string CVAR_CHECK_WEAPONS = .*/string CVAR_CHECK_WEAPONS = \"$countw\";/;
+	s/^string CVAR_CHECK_BALANCE = .*/string CVAR_CHECK_BALANCE = \"$countw\";/;
 " qcsrc/server/constants.qh > qcsrc/server/constants.qh.new
 
 if ! diff qcsrc/server/constants.qh qcsrc/server/constants.qh.new; then
