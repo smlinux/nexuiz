69ac2dd28b1f72345f41d37096b10e45f484df4f
div0 <div0@f962a42d-fe04-0410-a3ab-8c8b0445ebaa>
2009-10-03 18:07:16 +0000

    auto upload builds to offload

--- misc/tools/makebuild.sh
+++ misc/tools/makebuild.sh
@@ -338,6 +338,8 @@ ln -snf nexuizdebug$date$ext.zip "$zipdir/nexuizdebug-$newest.zip"
 
 case "$versiontag" in
 	test)
+		# these builds are public
+		rsync -vaSHP "$zipdir/nexuiz"*"-$date$ext.zip" "$zipdir/nexuiz"*"-$newest.zip" atoffload:offload.alientrap.org/nexuiz/builds/
 		;;
 	*)
 		cat <<'EOF'
