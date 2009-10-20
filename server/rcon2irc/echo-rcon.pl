edd06269e93cb23b82db95415c7b982d25bcba59
fruitiex <fruitiex@f962a42d-fe04-0410-a3ab-8c8b0445ebaa>
2009-10-20 17:58:55 +0000

    echo-rcon.pl now allows multiple instances of rcon2irc on the same server, bans.pl can echo connection attempts from banned clients (patch by merlijn)

--- server/rcon2irc/echo-rcon.pl
+++ server/rcon2irc/echo-rcon.pl
@@ -5,8 +5,15 @@ sub out($$@);
 
 [ dp => q{server received rcon command from (.*):  (.*)} => sub {
 	my ($origin, $cmd) = @_;
-	return 0 if ($origin eq $config{dp_listen}); #do not relay rcon2irc commands
+	my @dests = split ' ', $store{plugin_echo-rcon}->{dest};
+	return 0 if grep { $_ eq $origin } @dests; #do not relay rcon2irc commands
 	my $origin = color_dp2irc $origin;
 	out irc => 0, "PRIVMSG $config{irc_channel} :\00302* admin\017 command recieved from $origin: \00304$cmd\017";
+	return 0;
+} ],
+
+[ dp => q{"log_dest_udp" is "([^"]*)" \["[^"]*"\]} => sub {
+	my ($dest) = @_;
+	$store{plugin_echo-rcon}->{dest} = $dest;
 	return 0;
 } ],
